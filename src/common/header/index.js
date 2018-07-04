04c82abaa28f88dc1c2182280d97de498baa50db
张文文 <18622076193@163.com>
2018-07-04 15:19:04 +0800

    使用 iconfont 嵌入头部图标

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { HeaderWrapper, Logo, Nav, NavItem, NavSearch, Addition, Button } from './style'
+import { HeaderWrapper, Logo, SearchWrapper, Nav, NavItem, NavSearch, Addition, Button } from './style'
 
 class Header extends Component {
   render() {
@@ -9,11 +9,16 @@ class Header extends Component {
         <Nav>
           <NavItem className='left active'>首页</NavItem>
           <NavItem className='left'>下载App</NavItem>
-          <NavSearch/>
+          <SearchWrapper>
+            <NavSearch></NavSearch>
+            <i className="iconfont">&#xe614;</i>
+          </SearchWrapper>
           <NavItem className='right'>登陆</NavItem>
-          <NavItem className='right'>Aa</NavItem>
+          <NavItem className='right'>
+            <i className="iconfont">&#xe636;</i>
+          </NavItem>
           <Addition>
-            <Button className='writting'>写文章</Button>
+            <Button className='writting'><i className="iconfont">&#xe615;</i>写文章</Button>
             <Button className='reg'>注册</Button>
           </Addition>
         </Nav>
