4e38371d74ae00123ca028df28b9ef35dd0bd4ef
张文文 <18622076193@163.com>
2018-07-25 10:53:03 +0800

    使用 combineReducers 完成对数据的拆分管理

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -40,7 +40,7 @@ const Header = (props) =>{
 
 const mapStateToProps = (state) => {
   return {
-    focused: state.focused
+    focused: state.header.focused
   }
 }
 
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,19 +1,8 @@
-const defaultState = {
-  focused: false
-};
+import { combineReducers } from 'redux';
+import { reducer as headerReducer} from '../common/header/store'
 
-export default (state = defaultState, action) => {
-  if (action.type === 'search_focus') {
-    return{
-      focused: true
-    }
-  }
+const reducer = combineReducers({
+  header: headerReducer
+});
 
-  if (action.type === 'search_blur') {
-    return{
-      focused: false
-    }
-  }
-
-  return state;
-}
+export default reducer;
