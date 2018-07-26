5983a5303f75504452720045c59ca11d121c0f91
张文文 <18622076193@163.com>
2018-07-26 15:55:54 +0800

    避免无意义的请求发送，提升组件性能

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -56,7 +56,7 @@ class Header extends Component {
     }
   }
   render() {
-    const { focused, handleInputFocus, handleInputBlur } = this.props;
+    const { focused, list, handleInputFocus, handleInputBlur } = this.props;
     return (
       <HeaderWrapper>
         <Logo/>
@@ -71,7 +71,7 @@ class Header extends Component {
             >
               <NavSearch 
                 className={focused? 'focused' :''}
-                onFocus={handleInputFocus}
+                onFocus={()=>handleInputFocus(list)}
                 onBlur={handleInputBlur}
               >
               </NavSearch>
@@ -105,9 +105,9 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    handleInputFocus() {
-      dispatch(actionCreators.getList())
-      dispatch(actionCreators.searchFocus())
+    handleInputFocus(list) {
+      (list.size === 0) && dispatch(actionCreators.getList());
+			dispatch(actionCreators.searchFocus());
     },
     handleInputBlur() {
       dispatch(actionCreators.searchBlur())
