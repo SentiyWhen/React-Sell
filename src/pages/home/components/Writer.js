fcc99b7de392189e1bfdd6a72984e5712fe4bb10
张文文 <18622076193@163.com>
2018-07-27 14:51:07 +0800

    首页组件的拆分

---
+++ src/pages/home/components/Writer.js
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react';
+// import { WriterWrapper } from '../style';
+
+class Writer extends PureComponent {
+
+	render() {
+		return (
+			<div>HomeWork</div>
+		)
+	}
+}
+
+export default Writer;
