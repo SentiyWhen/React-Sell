196f7351baff7ab403d4422ddeb7f680850306d4
张文文 <18622076193@163.com>
2018-07-30 14:33:53 +0800

    首页推荐部分代码编写

--- src/pages/home/components/Recommend.js
+++ src/pages/home/components/Recommend.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { RecommendWrapper, RecommendItem } from '../style';
 
 class Recommend extends Component {
 
 	render() {
+    const { list } = this.props;
 		return (
-			<div>Recommend</div>
+			<RecommendWrapper>
+        
+        {list.map((item)=>{
+          return <RecommendItem key={item.get('id')} imgUrl={item.get('imgUrl')} />
+        })}
+
+      </RecommendWrapper>
 		)
 	}
 }
+const mapState = (state) =>({
+  list: state.getIn(['home','recommendList'])
+})
 
-export default Recommend;
+export default connect(mapState,null)(Recommend);
--- src/pages/home/components/Writer.js
+++ src/pages/home/components/Writer.js
@@ -1,11 +1,11 @@
 import React, { PureComponent } from 'react';
-// import { WriterWrapper } from '../style';
+import { WriterWrapper } from '../style';
 
 class Writer extends PureComponent {
 
 	render() {
 		return (
-			<div>HomeWork</div>
+			<WriterWrapper>HomeWork</WriterWrapper>
 		)
 	}
 }
