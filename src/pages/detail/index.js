ae0a4823761a3808e95c5bd7f70cf6c0dcd63f69
张文文 <18622076193@163.com>
2018-07-31 14:10:43 +0800

    使用redux管理详情页面数据

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,20 +1,35 @@
-import React, { Component } from 'react'
+import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import { DetailWrapper, Header, Content } from './style';
 import { actionCreators } from './store';
 
-class Detail extends Component {
-  render() {
-    return(
-      <DetailWrapper>
-        <Header>jaychou</Header>
-        <Content>
-        jaychou
-        </Content>
-      </DetailWrapper>
-    )
-  }
+class Detail extends PureComponent {
+	render() {
+		return (
+			<DetailWrapper>
+				<Header>{this.props.title}</Header>
+				<Content 
+					dangerouslySetInnerHTML={{__html: this.props.content}}
+				/>
+			</DetailWrapper>
+		)
+	}
+
+	componentDidMount() {
+		this.props.getDetail(this.props.match.params.id);
+	}
 }
 
-export default Detail
+const mapState = (state) => ({
+	title: state.getIn(['detail', 'title']),
+	content: state.getIn(['detail', 'content'])
+});
+
+const mapDispatch = (dispatch) => ({
+	getDetail(id) {
+		dispatch(actionCreators.getDetail(id));
+	}
+});
+
+export default connect(mapState, mapDispatch)(withRouter(Detail));
