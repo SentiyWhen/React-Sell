9e6dbedcd7879251198c6c5ad76ad7a1dd55a554
张文文 <18622076193@163.com>
2019-03-29 09:57:32 +0800

    来个简单的todolist

---
+++ todolist/src/Todolist.js
@@ -0,0 +1,19 @@
+import React, { Component, Fragment } from 'react';
+
+class Todolist extends Component {
+  render() {
+    return (
+      <Fragment>
+        <input></input>
+        <button>add</button>
+        <ul>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+        </ul>
+      </Fragment>
+    );
+  }
+}
+
+export default Todolist;
