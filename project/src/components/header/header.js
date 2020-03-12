6d363de5a7d796b18283453597bcbc5544c9f33c
张文文 <zhangwwe@seentao.com>
2020-03-12 16:46:49 +0800

    feat: CSS+Sticky+footer布局

--- project/src/components/header/header.js
+++ project/src/components/header/header.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, {useState} from "react";
 import styles from './header.less';
 import classnames from "classnames";
 
 function Header(data) {
   const { seller } = data;
   const classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
+  const [detailShow, setdetailShow] = useState(false)
 
   return (
     <div className={styles.header}>
@@ -20,21 +21,21 @@ function Header(data) {
           <div className={styles.description}>
             {seller.description}/{seller.deliveryTime}分钟送达
           </div>
-          { seller.supports ?
+          { seller.supports &&
           <div className={styles.support}>
             <span className={classnames(styles.icon,styles[classMap[seller.supports[0].type]])}></span>
             <span className={styles.text}>{seller.supports[0].description}</span>
           </div>
-          : null }
+          }
         </div>
-        { seller.supports ?
-        <div className={styles.support_count}>
+        { seller.supports &&
+        <div className={styles.support_count} onClick={()=>setdetailShow(!detailShow)}>
           <span className={styles.count}>{seller.supports.length}个</span>
           <i className="icon-keyboard_arrow_right"></i>
         </div>
-        : null }
+        }
       </div>
-      <div className={styles.bulletin_wrapper}>
+      <div className={styles.bulletin_wrapper} onClick={()=>setdetailShow(!detailShow)}>
         <span className={styles.bulletin_title}></span>
         <span className={styles.bulletin_text}>{seller.bulletin}</span>
         <i className="icon-keyboard_arrow_right"></i>
@@ -42,6 +43,18 @@ function Header(data) {
       <div className={styles.background}>
         <img alt="" src={seller.avatar}></img>
       </div>
+      {detailShow && 
+      <div className={styles.detail}>
+        <div className={classnames(styles.detail_wrapper,'clearfix')}>
+          <div className={styles.detail_main}>
+            <p>{seller.bulletin}</p>
+          </div>
+        </div>
+        <div className={styles.detail_close}>
+          <i className="icon-close"></i>
+        </div>
+      </div>
+      }
     </div>
   );
 }
